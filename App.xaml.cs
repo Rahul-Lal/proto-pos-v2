80526c935d32e2f7182b3be3d63c5a92626d645b
Rahul Lal <Rahul21Lal@gmail.com>
2026-03-26 21:18:22 +1300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace proto_pos_v2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
