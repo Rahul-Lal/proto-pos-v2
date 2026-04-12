0cad2f9fd6668d7e453e523dc1f330ebceb2a3fc
Rahul Lal <Rahul21Lal@gmail.com>
2026-04-13 08:51:17 +1200

    Initialise StaffMealWindow

---
+++ StaffMealWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proto_pos_v2
+{
+    /// <summary>
+    /// Interaction logic for StaffMealWindow.xaml
+    /// </summary>
+    public partial class StaffMealWindow : Window
+    {
+        public StaffMealWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
