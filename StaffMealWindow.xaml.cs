56ada646dfc9367d99638af5bd4c3a1288192ab9
Rahul Lal <Rahul21Lal@gmail.com>
2026-04-13 08:55:46 +1200

    Staff meal buttons have methods

--- StaffMealWindow.xaml.cs
+++ StaffMealWindow.xaml.cs
@@ -17,9 +17,78 @@ namespace proto_pos_v2
     /// </summary>
     public partial class StaffMealWindow : Window
     {
-        public StaffMealWindow()
+        private readonly HomePage _home;
+
+        public StaffMealWindow(HomePage home)
         {
             InitializeComponent();
+            _home = home;
+        }
+
+        private void printStaffMeal(string burger)
+        {
+            string chosenCombo;
+
+            chosenCombo = burger.ToUpper() + " STAFF COMBO\n" +
+                    burger + " Burger \n" +
+                    "Medium Fries \n" +
+                    "Medium Drink \n";
+
+            _home.txtOutput.Text += chosenCombo;
+            _home.txtPrices.Text += "$0.00";
+            _home.total += 0.00;
+            _home.isStaffMealSelected = true;
+            this.Close();
+        }
+
+        private void btnSingleOlympian_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Single Olympian");
+        }
+
+        private void btnSingleParisan_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Single Parisian");
+        }
+
+        private void btnSingleRoma_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Single Roma");
+        }
+
+        private void btnNashvilleHot_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Nashville Hot");
+        }
+
+        private void btnKyotoKatsu_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Kyoto Katsu");
+        }
+
+        private void btnMarrakesh_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Marrakesh Chicken");
+        }
+
+        private void btnOaxaca_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Oaxaca Veggie");
+        }
+
+        private void btnBombay_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Bombay Veggie");
+        }
+
+        private void btnNordic_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Nordic Fish");
+        }
+
+        private void btnHavana_Click(object sender, RoutedEventArgs e)
+        {
+            printStaffMeal("Havana Fish");
         }
     }
 }
