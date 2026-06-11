1e515046eccdc80abbf22bddf594c47bdcdd24ba
Rahul Lal <Rahul21Lal@gmail.com>
2026-06-12 00:30:32 +1200

    PaymentWindow shows via homepage.

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -783,7 +783,7 @@ namespace proto_pos_v2
         private void btnPayment_Click(object sender, RoutedEventArgs e)
         {
             var paymentWindow = new PaymentWindow(this, total);
-            //paymentWindow.ShowDialog();
+            paymentWindow.ShowDialog();
         }
 
         private void btnUndo_Click(object sender, RoutedEventArgs e)
